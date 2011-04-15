d78a9547ffe93a389a9ac8e634cdb00c534d9e94
Sven van Haastregt <svhaastr@liacs.nl>
2011-04-15 11:50:14 +0000

    Cleanup of FIFO

--- src/espam/libSystemC/fifo_fsl.h
+++ src/espam/libSystemC/fifo_fsl.h
@@ -2,14 +2,19 @@
  * Timed FIFO
  * Sven van Haastregt
  * LERC, LIACS, Leiden University
+ * Version: $Id: fifo_fsl.h,v 1.3 2011/04/15 11:50:14 svhaastr Exp $
  */
-#ifndef _FIFO_P2P_H_
-#define _FIFO_P2P_H_
+#ifndef _FIFO_FSL_H_
+#define _FIFO_FSL_H_
 
 #include "systemc.h"
 
-// TODO: does support back-to-back reads, but not in a proper way (read immediately returns)
-//
+// Usage notes:
+// Each cycle only one read and one write operation are allowed. If data is read/written (that is, the operation doesn't block
+// on an empty or full FIFO), time is not advanced: advancing time should be done by the caller.
+
+// Set to nonzero to print FIFO operations
+//#define FIFO_VERBOSE 1
 
 template <class T>
 SC_MODULE(fsl) , public sc_fifo<T> {
@@ -45,7 +50,7 @@ SC_MODULE(fsl) , public sc_fifo<T> {
     int read_latency;     // Time between read call and return (simulates time to fetch data from buffer)
     int write_latency;    // Time between write call and actual write into buffer; write() returns immediately if not blocking! Multiple writes in subsequent cycles are pipelined.
     int flen;             // Resembling fsl.fifo_length
-    int max_tokens;
+    int max_tokens;       // Maximum number of tokens simultaneously in FIFO (collected during execution)
 };
 
 
@@ -74,8 +79,8 @@ fsl<T>::~fsl() {
 template <class T>
 void fsl<T>::init(int size, sc_trace_file *tf) {
   read_pending = -1;
-  read_latency = 1;
-  write_latency = 1;
+  read_latency = 1;   // Values != 1 not tested
+  write_latency = 1;  // Values != 1 not tested
   SC_CTHREAD(fsl_process, clk.pos());
   this->read_pipeline = new bool[read_latency+1];
   this->read_queue = new T[read_latency+1];
@@ -112,15 +117,16 @@ template <class T>
 void fsl<T>::read( T& val_ )
 {
   if (this->read_pending == 1) {
-    cout << sc_module::name() << ": Warning: pending read will be overwritten!" << endl;
+    cerr << "[" << sc_module::name() << "] Error: more than one read during cycle " << sc_time_stamp() << endl;
+    exit(1);
   }
 
   // Block if empty
   while( this->num_available() == 0 ) {
+#ifdef FIFO_VERBOSE
     cout << sc_module::name() << ": Blocking read at " << sc_time_stamp() << endl;
+#endif
     sc_core::wait(1, SC_NS);
-    //sc_core::wait( this->m_data_written_event );
-    //cout << sc_module::name() << ": waking up at " << sc_time_stamp() << endl;
   }
 
   // Do actual read in next clock cycle
@@ -129,21 +135,25 @@ void fsl<T>::read( T& val_ )
   this->m_num_read++;
   this->buf_read(this->read_val);
   this->request_update();
-  //sc_core::wait(this->read_done);
 
   val_ = read_val;
 }
 
 template <class T>
 void fsl<T>::write(const T& val) {
+#ifdef FIFO_VERBOSE
   cout << "Write " << sc_module::name() << " at " << sc_time_stamp() << endl;
+#endif
   if (write_pipeline[0] == true) {
-    cout << sc_module::name() << ": Warning: pending write will be overwritten! " << endl;
+    cerr << "[" << sc_module::name() << "] Error: more than one write during cycle " << sc_time_stamp() << endl;
+    exit(1);
   }
 
   // Block if full
   while (this->num_free() == 0) {
+#ifdef FIFO_VERBOSE
     cout << sc_module::name() << ": Blocking write at " << sc_time_stamp() << endl;
+#endif
     // TODO: shouldn't this be a wait 1 SC_NS as well..?
     sc_core::wait( this->m_data_read_event );
     //sc_core::wait(1, SC_NS);
@@ -152,18 +162,6 @@ void fsl<T>::write(const T& val) {
   // Queue the write operation
   write_queue[0] = val;
   write_pipeline[0] = true;
-/*  if (this->write_pending >= 0) {
-    cout << sc_module::name() << ": Warning: pending write will be overwritten! " << sc_time_stamp() << endl;
-  }
-
-  // Block if full
-  while (this->num_free() == 0) {
-    sc_core::wait( this->m_data_read_event );
-  }
-
-  // Keep value and do actual write in next clock cycle
-  this->write_pending = write_latency;
-  this->write_val = val;*/
 }
 
 
@@ -177,9 +175,6 @@ void fsl<T>::fsl_process() {
       write_queue[i] = write_queue[i-1];
     }
     write_pipeline[0] = false;
-
-    //exist.write(this->num_available() != 0);
-    //full.write(this->num_free() == 0);
 
     if (this->read_pending != 1  &&  write_pipeline[write_latency] == true  &&  full.read()==false) {
       // write and no read: increment
@@ -200,177 +195,20 @@ void fsl<T>::fsl_process() {
     if (this->read_pending > 0) {
       this->read_pending--;
     }
-    /*else if (this->read_pending == 0) {
-      //cout << sc_module::name() << " committing read at " << sc_time_stamp() << endl;
-      this->read_pending = -1;
-      this->m_num_read++;
-      this->buf_read(this->read_val);
-      read_done.notify(SC_ZERO_TIME);
-      this->request_update();
-      //exist.write(this->m_size - this->m_free != 0);
-      //full.write(this->m_free == 0);
-    }*/
-/*
-    // Advance read queue
-    for (int i = read_latency; i > 0; i--) {
-      read_pipeline[i] = read_pipeline[i-1];
-      read_queue[i] = read_queue[i-1];
-    }
-    read_pipeline[0] = false;
-    if (read_pipeline[read_latency] == true) {
-      // A read is coming out of the queue...:
-    }*/
 
     if (write_pipeline[write_latency] == true) {
       // A write is coming out of the queue, commit it:
+#ifdef FIFO_VERBOSE
       cout << sc_module::name() << " committing write at " << sc_time_stamp() << endl;
+#endif
       this->m_num_written++;
       this->buf_write(write_queue[write_latency]);
       this->request_update();
-      //exist.write(this->m_size - this->m_free != 0);
-      //full.write(this->num_free() == 0);
     }
 
-    // Handle write
-/*    if (this->write_pending > 0) {
-      this->write_pending--;
-    }
-    else if (this->write_pending == 0) {
-      cout << sc_module::name() << " committing write at " << sc_time_stamp() << endl;
-      this->write_pending = -1;
-      this->m_num_written++;
-      this->buf_write(this->write_val);
-      this->request_update();
-    }*/
-    //full.write(this->m_free == 0);
-    //exist.write(this->m_size - this->m_free != 0);
     sc_core::wait();
   }
 }
 
 
-/*
-template <class T>
-SC_MODULE(fifo) , public sc_fifo_in_if<T>,
-                  public sc_fifo_out_if<T> {
-  public:
-    sc_in<bool> clk;
-    sc_in<bool> rst;
-    //sc_fifo_in<T> in;
-    //sc_fifo_out<T> out;
-    //sc_port<sc_fifo_in_if<T> > in;
-    //sc_port<sc_fifo_out_if<T> > out;
-    //sc_in<T> slave;
-    //sc_out<T> master;
-
-    fifo(sc_module_name mn, int size);
-    ~fifo();
-
-    T read();
-    void read(T&);
-    bool nb_read( T& );
-    void write(const T&);
-    bool nb_write(const T&);
-
-    int num_available() const;
-    int num_free() const;
-
-    const sc_event& data_read_event() const;
-    const sc_event& data_written_event() const;
-
-    SC_HAS_PROCESS(fifo);
-
-  private:
-    T *buffer;
-
-  protected:
-    sc_event m_data_read_event;
-    sc_event m_data_written_event;
-};
-
-
-template <class T>
-fifo<T>::fifo(sc_module_name mn, int size) {
-//  SC_THREAD(read);
-//  sensitive << slave;
-
-  //SC_THREAD(write);
-  //sensitive << slave;
-  //dont_initialize();
-  buffer = new T[size];
-}
-
-
-template <class T>
-fifo<T>::~fifo() {
-  delete[] buffer;
-}
-
-
-template<class T>
-T fifo<T>::read() {
-  return NULL;
-}
-
-template<class T>
-void fifo<T>::read(T&) {
-}
-
-template<class T>
-bool fifo<T>::nb_read( T& ) {
-}
-
-template<class T>
-void fifo<T>::write(const T&) {
-}
-
-template<class T>
-bool fifo<T>::nb_write(const T&) {
-}
-
-
-template<class T>
-int fifo<T>::num_available() const {
-  return 0;
-}
-
-template<class T>
-int fifo<T>::num_free() const {
-  return 0;
-}
-
-
-template<class T>
-const sc_event& fifo<T>::data_read_event() const {
-  return m_data_read_event;
-}
-
-template<class T>
-const sc_event& fifo<T>::data_written_event() const {
-  return m_data_written_event;
-}
-*/
-
-/*template<class T>
-class fifo_p2p: public sc_fifo<T> {
-  public:
-    fifo_p2p(int size) :
-      sc_fifo<T> (size) {
-    }
-
-    ~fifo_p2p() {
-    }
-
-    void write( const T& val ) {
-      sc_fifo<T>::write(val);
-    }
-
-    T read() {
-      return sc_fifo<T>::read();
-    }
-
-    sc_in<bool> clk;
-    sc_in<bool> rst;
-};
-*/
 #endif
