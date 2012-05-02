ef8cf7b093f10499ee01473593cc9c05af5921f5
Teddy Zhai <tzhai@liacs.nl>
2012-05-02 16:25:45 +0000

    added an option to use local variable implementing self channels

---
+++ src/espam/visitor/xps/cdpn/Options.java
@@ -0,0 +1,32 @@
+/*******************************************************************\
+
+The ESPAM Software Tool 
+Copyright (c) 2004-2011 Leiden University (LERC group at LIACS).
+All rights reserved.
+
+The use and distribution terms for this software are covered by the 
+Common Public License 1.0 (http://opensource.org/licenses/cpl1.0.txt)
+which can be found in the file LICENSE at the root of this distribution.
+By using this software in any fashion, you are agreeing to be bound by 
+the terms of this license.
+
+You must not remove this notice, or any other, from this software.
+
+\*******************************************************************/
+
+package espam.visitor.xps.cdpn;
+
+//////////////////////////////////////////////////////////////////////////
+//// 
+
+/**
+ * 
+ *
+ * @author  Teddy Zhai
+ * @version  $Id: Options.java,v 1.1 2012/05/02 16:25:45 tzhai Exp $
+ */
+
+public class Options {
+	// use local variables for self channels
+	public static boolean USE_LOCAL_VAR_FIFO = false;
+}
