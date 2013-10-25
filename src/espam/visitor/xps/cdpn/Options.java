02d3627e8b63fdf1736c887b250ea82e37d49aac
Jelena Spasic <jspasic@liacs.nl>
2013-10-25 10:56:25 +0200

    Added a flag to control SDK full automation

--- src/espam/visitor/xps/cdpn/Options.java
+++ src/espam/visitor/xps/cdpn/Options.java
@@ -5,13 +5,14 @@ package espam.visitor.xps.cdpn;
 //// 
 
 /**
- * 
- *
- * @author  Teddy Zhai
- * @version  $Id: Options.java,v 1.1 2012/05/02 16:25:45 tzhai Exp $
- */
+* 
+*
+* @author  Teddy Zhai
+* @version  $Id: Options.java,v 1.1 2012/05/02 16:25:45 tzhai Exp $
+*/
 
 public class Options {
     // use local variables for self channels
     public static boolean USE_LOCAL_VAR_FIFO = true;
+    public static boolean USE_FULLY_AUTOMATED_SDK = false;
 }
